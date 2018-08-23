3ba5ef5d8893cc923bc0abc909635d12cc2ab301
Tom Byers <tom.byers@digital.cabinet-office.gov.uk>
2018-08-23 17:03:31 +0100

    Add basic comparator with a wrapper for rules

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-const eslintRules = require('./lib/eslint-rules');
+const fs = require('fs');
+const path = require('path');
 
-console.log(eslintRules);
+const Papa = require('papaparse');
+
+const Comparator = require('./lib/comparator.js');
+
+const coreRules = require('./lib/eslint-rules.js');
+const standardConfig = require('eslint-config-standard');
+const xoConfig = require('eslint-config-xo');
+
+
+const comparator = new Comparator(coreRules.asHash, standardConfig.rules, xoConfig.rules);
+
+console.log(comparator.compare());
